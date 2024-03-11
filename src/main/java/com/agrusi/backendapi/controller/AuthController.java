b0d5230b3226eb1bce0d7c6eccde86d417e0d533
Juhani Rusi <juhanirusi@yahoo.com>
2024-03-11 16:58:18 +0200

    Added the Authentication controller.

---
+++ src/main/java/com/agrusi/backendapi/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.agrusi.backendapi.controller;
+
+import com.agrusi.backendapi.dto.RegisterDto;
+import com.agrusi.backendapi.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/auth")
+public class AuthController {
+
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    // Register new user REST API endpoint...
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+
+        String response = authService.register(registerDto);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+}
