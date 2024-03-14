64e6e856493d96631a0d29978932c28656900bd1
Juhani Rusi <juhanirusi@yahoo.com>
2024-03-14 19:40:57 +0200

    Added "AuthGlobalExceptionHandler" with overriden "handleMethodArgumentNotValid" exception.

---
+++ src/main/java/com/agrusi/backendapi/exception/AuthGlobalExceptionHandler.java
@@ -0,0 +1,53 @@
+package com.agrusi.backendapi.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.*;
+
+@ControllerAdvice
+public class AuthGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    /**
+    * Our overridden "handleMethodArgumentNotValid" exception to throw
+     * more "beautiful and useful validation error responses to the user
+    */
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+
+        List<Map<String, String>> errorDetails = new ArrayList<>();
+
+        for (FieldError error : fieldErrors) {
+            Map<String, String> errorMap = new LinkedHashMap<>();
+            errorMap.put("field", error.getField());
+            errorMap.put("reason", error.getDefaultMessage());
+            errorDetails.add(errorMap);
+        }
+
+        Map<String, Object> errorResponse = new LinkedHashMap<>();
+
+        errorResponse.put("timestamp", new Date());
+        errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
+        errorResponse.put("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
+        errorResponse.put("message", "Validation failed");
+        errorResponse.put("path", ((ServletWebRequest)request).getRequest().getRequestURI());
+        errorResponse.put("errors", errorDetails);
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
