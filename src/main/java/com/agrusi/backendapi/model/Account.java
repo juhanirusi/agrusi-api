62702f626ab76580ff0577ef05a3cfb745de44d7
Juhani Rusi <juhanirusi@yahoo.com>
2024-03-22 17:54:44 +0200

    Edited Account entity by adding 2 new fields for first and last name and added "getFullName" helper method.

--- src/main/java/com/agrusi/backendapi/model/Account.java
+++ src/main/java/com/agrusi/backendapi/model/Account.java
@@ -17,6 +17,12 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
     @Email
     @Column(name = "email", nullable = false, unique = true)
     private String email;
@@ -33,16 +39,20 @@ public class Account {
     private LocalDateTime dateCreated;
 
     @UpdateTimestamp
-    @Column(name = "last_updated")
+    @Column(name = "last_updated", nullable = false)
     private LocalDateTime lastUpdated;
 
     public Account() {
     }
 
     public Account(
-            String email, String password, boolean isVerified,
-            LocalDateTime dateCreated, LocalDateTime lastUpdated
+            String firstName, String lastName,
+            String email, String password,
+            boolean isVerified, LocalDateTime dateCreated,
+            LocalDateTime lastUpdated
     ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.isVerified = isVerified;
@@ -62,8 +72,24 @@ public class Account {
     * an important field.
     */
 
-    public void setId(UUID id) {
+    protected void setId(UUID id) {
         this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
@@ -104,6 +130,12 @@ public class Account {
 
     public void setLastUpdated(LocalDateTime lastUpdated) {
         this.lastUpdated = lastUpdated;
+    }
+
+    // Get user's full name
+
+    public String getFullName() {
+        return firstName + " " + lastName;
     }
 }
 
