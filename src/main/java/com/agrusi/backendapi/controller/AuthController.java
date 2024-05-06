f121c0baaaabbce3c394b3fe11f01639bc3b41b7
Juhani Rusi <juhanirusi@yahoo.com>
2024-05-06 19:51:32 +0300

    Added controllers for user registration and login.

--- src/main/java/com/agrusi/backendapi/controller/AuthController.java
+++ src/main/java/com/agrusi/backendapi/controller/AuthController.java
@@ -1,32 +1,94 @@
 package com.agrusi.backendapi.controller;
 
-import com.agrusi.backendapi.dto.RegisterDto;
+import com.agrusi.backendapi.dto.request.LoginDto;
+import com.agrusi.backendapi.dto.request.RegisterDto;
+import com.agrusi.backendapi.dto.response.LoginResponseDto;
+import com.agrusi.backendapi.dto.response.RegisterAccountResponseDto;
+import com.agrusi.backendapi.exception.auth.AccountWithEmailAlreadyExistException;
+import com.agrusi.backendapi.handler.ResponseHandler;
 import com.agrusi.backendapi.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/auth")
 public class AuthController {
 
-    private AuthService authService;
+    private final AuthService authService;
 
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
 
-    // Register new user REST API endpoint...
+    // Create (register) a new user REST API endpoint...
 
-    @PostMapping(value = "/register")
-    public ResponseEntity<String> register(@Valid @RequestBody RegisterDto registerDto
-    ) {
-        String response = authService.register(registerDto);
+    @PostMapping(value = "/register") // PERHAPS REPLACE <Object> WITH <?>
+    public ResponseEntity<Object> register(@Valid @RequestBody RegisterDto registerDto) {
 
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+        try {
+            RegisterAccountResponseDto createdAccount = authService.registerNewAccount(registerDto);
+
+            return ResponseHandler.generateResponse(
+                    HttpStatus.CREATED,
+                    "success",
+                    "Account created successfully.",
+                    new HashMap<>() {{
+                        put("id", createdAccount.id());
+                        put("email", createdAccount.email());
+                        put("firstName", createdAccount.firstName());
+                        put("lastName", createdAccount.lastName());
+                        put("accountVerified", createdAccount.accountVerified());
+                        put("dateCreated", createdAccount.dateCreated());
+                    }}
+            );
+        } catch (AccountWithEmailAlreadyExistException exception) {
+
+            return ResponseHandler.generateResponse(
+                    HttpStatus.CONFLICT,
+                    "error",
+                    null,
+                    Map.of(
+                            "message", exception.getMessage()
+                    )
+            );
+        }
+    }
+
+    // Log in an existing user REST API endpoint...
+
+    @PostMapping(value = "/login")
+    public ResponseEntity<Object> login(@Valid @RequestBody LoginDto loginDto) {
+
+        LoginResponseDto loggedInAccount = authService.login(loginDto);
+
+        /**
+         * REMEMBER TO DO ERROR HANDLING AS WELL !!!
+         */
+
+        return ResponseHandler.generateResponse(
+                HttpStatus.OK,
+                "success",
+                "Login successful.",
+                new HashMap<>() {{
+                    put("email", loggedInAccount.email());
+                    put("accessToken", loggedInAccount.token());
+                }}
+        );
+    }
+
+    @GetMapping(value = "/secured")
+    public String secured() {
+        return "YOU GET TO THE PAGE";
+    }
+
+    @GetMapping("/user")
+    public Principal user(Principal user) {
+        return user;
     }
 }
