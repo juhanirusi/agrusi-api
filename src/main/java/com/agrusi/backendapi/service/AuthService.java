7462c2d920e6c0145c686f1519561d11ff732fc2
Juhani Rusi <juhanirusi@yahoo.com>
2024-05-06 19:53:21 +0300

    Added interface methods for registering a new account and logging in an existing user.

--- src/main/java/com/agrusi/backendapi/service/AuthService.java
+++ src/main/java/com/agrusi/backendapi/service/AuthService.java
@@ -1,9 +1,13 @@
 package com.agrusi.backendapi.service;
 
-import com.agrusi.backendapi.dto.RegisterDto;
+import com.agrusi.backendapi.dto.request.LoginDto;
+import com.agrusi.backendapi.dto.request.RegisterDto;
+import com.agrusi.backendapi.dto.response.LoginResponseDto;
 import com.agrusi.backendapi.dto.response.RegisterAccountResponseDto;
 
 public interface AuthService {
 
-    RegisterAccountResponseDto register(RegisterDto registerDto);
+    RegisterAccountResponseDto registerNewAccount(RegisterDto registerDto);
+
+    LoginResponseDto login(LoginDto loginDto);
 }
