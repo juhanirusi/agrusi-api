19b4aec535e11711c8d00472aeadabb263814657
Juhani Rusi <juhanirusi@yahoo.com>
2024-05-06 19:52:37 +0300

    Edited the exception handler to make the ApiError simpler and remove unnecessary code.

--- src/main/java/com/agrusi/backendapi/exception/auth/AuthGlobalExceptionHandler.java
+++ src/main/java/com/agrusi/backendapi/exception/auth/AuthGlobalExceptionHandler.java
@@ -8,11 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class AuthGlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -21,36 +23,6 @@ public class AuthGlobalExceptionHandler extends ResponseEntityExceptionHandler {
     * Our overridden "handleMethodArgumentNotValid" exception to throw
      * more user-friendly and useful validation error responses to the user
     */
-
-//    @Override
-//    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-//            MethodArgumentNotValidException exception,
-//            HttpHeaders headers,
-//            HttpStatusCode status,
-//            WebRequest request
-//    ) {
-//        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
-//
-//        List<Map<String, String>> errorDetails = new ArrayList<>();
-//
-//        for (FieldError error : fieldErrors) {
-//            Map<String, String> errorMap = new LinkedHashMap<>();
-//            errorMap.put("field", error.getField());
-//            errorMap.put("reason", error.getDefaultMessage());
-//            errorDetails.add(errorMap);
-//        }
-//
-//        Map<String, Object> errorResponse = new LinkedHashMap<>();
-//
-//        errorResponse.put("timestamp", new Date());
-//        errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
-//        errorResponse.put("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
-//        errorResponse.put("message", "Validation failed");
-//        errorResponse.put("path", ((ServletWebRequest)request).getRequest().getRequestURI());
-//        errorResponse.put("errors", errorDetails);
-//
-//        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-//    }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
@@ -71,10 +43,8 @@ public class AuthGlobalExceptionHandler extends ResponseEntityExceptionHandler {
         }
 
         ApiError apiError = new ApiError(
-                new Date(),
-                status.value(),
+                "error",
                 "Validation failed",
-                ((ServletWebRequest)request).getRequest().getRequestURI(),
                 errorDetails
         );
 
