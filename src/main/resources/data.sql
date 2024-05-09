583c14844f018ffbe4203859bfd2ed195cc4f623
Juhani Rusi <juhanirusi@yahoo.com>
2024-05-09 21:38:00 +0300

    Added an SQL file to run when we want to add initial data to the database.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO role (role_id, authority) VALUES (1, 'ADMIN');
-INSERT INTO role (role_id, authority) VALUES (2, 'MODERATOR');
-INSERT INTO role (role_id, authority) VALUES (3, 'USER');
+INSERT INTO role (id, authority) VALUES (1, 'ADMIN');
+INSERT INTO role (id, authority) VALUES (2, 'MODERATOR');
+INSERT INTO role (id, authority) VALUES (3, 'USER');
