875010023198eaf878301b879a6d5611a9711aa5
Juhani Rusi <juhanirusi@yahoo.com>
2024-05-24 21:38:13 +0300

    A little fix to the RoleRepository class.

--- src/main/java/com/agrusi/backendapi/repository/RoleRepository.java
+++ src/main/java/com/agrusi/backendapi/repository/RoleRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Integer> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 
     Optional<Role> findByAuthority(EAccountRole name);
 }
