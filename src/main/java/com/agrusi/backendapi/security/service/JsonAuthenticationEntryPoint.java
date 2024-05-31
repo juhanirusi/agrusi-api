1e9e5d39e31de3dc31ad22cd9c21e7f4bb5b0400
Juhani Rusi <juhanirusi@yahoo.com>
2024-05-31 21:11:19 +0300

    Added JsonAuthenticationEntryPoint class, which is a custom implementation of the AuthenticationEntryPoint needed to send custom responses on authentication failure.

---
+++ src/main/java/com/agrusi/backendapi/security/service/JsonAuthenticationEntryPoint.java
@@ -0,0 +1,40 @@
+package com.agrusi.backendapi.security.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.*;
+
+@Component
+public class JsonAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException {
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        List<Map<String, String>> errors = new ArrayList<>();
+        Map<String, String> error = new HashMap<>();
+        error.put("message", "Invalid credentials");
+        errors.add(error);
+
+        Map<String, Object> responseData = new LinkedHashMap<>();
+        responseData.put("status", "error");
+        responseData.put("message", "Login failed");
+        responseData.put("error", errors);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(response.getWriter(), responseData);
+    }
+}
