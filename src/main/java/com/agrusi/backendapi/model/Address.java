4a6357a8a3f33b7b8a70ed3000733dd455b81b74
Juhani Rusi <juhanirusi@yahoo.com>
2024-06-09 18:28:03 +0300

    Added the abstract class Address that we can use in the future to hold Account or Farm address information, for example.

---
+++ src/main/java/com/agrusi/backendapi/model/Address.java
@@ -0,0 +1,98 @@
+package com.agrusi.backendapi.model;
+
+import jakarta.persistence.*;
+
+@MappedSuperclass
+public abstract class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(updatable = false, nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "street_address", nullable = false)
+    private String streetAddress;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "province", nullable = false)
+    private String province;
+
+    @Column(name = "postal_code", nullable = false)
+    private String postalCode;
+
+    @Column(name = "country", nullable = false)
+    private String country;
+
+    public Address() {
+    }
+
+    public Address(
+            Long id,
+            String streetAddress,
+            String city,
+            String province,
+            String postalCode,
+            String country
+    ) {
+        this.id = id;
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.province = province;
+        this.postalCode = postalCode;
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    /*
+     * Primary key values never change, so you shouldn't allow the
+     * identifier property value to be modified. Hibernate and Spring
+     * Data JPA using Hibernate as a provider won’t update a primary
+     * key column, and you shouldn't expose a public identifier
+     * setter method on an entity!
+    */
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
