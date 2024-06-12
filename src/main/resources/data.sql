5f6ccb19a95bc05b118df7f2bfa8169e194b184f
Juhani Rusi <juhanirusi@yahoo.com>
2024-06-12 20:10:01 +0300

    Updated the data.sql, so that if roles already exist, we'll do nothing.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO role (id, authority) VALUES (1, 'ADMIN');
-INSERT INTO role (id, authority) VALUES (2, 'MODERATOR');
-INSERT INTO role (id, authority) VALUES (3, 'USER');
+INSERT INTO role (id, authority) VALUES (1, 'ADMIN') ON CONFLICT (id) DO NOTHING;
+INSERT INTO role (id, authority) VALUES (2, 'MODERATOR') ON CONFLICT (id) DO NOTHING;
+INSERT INTO role (id, authority) VALUES (3, 'USER') ON CONFLICT (id) DO NOTHING;
