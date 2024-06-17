050b8e9d004cf399ddf50e9b9f7687aafb051ea5
Juhani Rusi <juhanirusi@yahoo.com>
2024-06-17 20:15:36 +0300

    Edited the exception class by hardcoding the message to the exception and added a new message containing the email used.

--- src/main/java/com/agrusi/backendapi/exception/auth/AccountWithEmailAlreadyExistException.java
+++ src/main/java/com/agrusi/backendapi/exception/auth/AccountWithEmailAlreadyExistException.java
@@ -2,7 +2,11 @@ package com.agrusi.backendapi.exception.auth;
 
 public class AccountWithEmailAlreadyExistException extends RuntimeException {
 
-    public AccountWithEmailAlreadyExistException(final String message) {
-        super(message);
+    public AccountWithEmailAlreadyExistException(final String email) {
+        super("Account with email '" + email + "' already exists");
+    }
+
+    public AccountWithEmailAlreadyExistException() {
+        super("Account with this email already exists");
     }
 }
