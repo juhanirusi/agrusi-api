54f1904c77d79c6fef8a5c16efe223f305c0d05e
Juhani Rusi <juhanirusi@yahoo.com>
2024-06-17 20:16:21 +0300

    Just a little edit to the message of the validation.

--- src/main/java/com/agrusi/backendapi/validator/annotation/ValidPassword.java
+++ src/main/java/com/agrusi/backendapi/validator/annotation/ValidPassword.java
@@ -17,10 +17,9 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 public @interface ValidPassword {
 
-    String message() default "Invalid Password";
+    String message() default "Invalid Password.";
 
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
-
 }
