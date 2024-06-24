885259919a70993a02f11c72646c36ef9d586bd7
Juhani Rusi <juhanirusi@yahoo.com>
2024-06-24 20:13:23 +0300

    Streamlined the "ResponseHandler" by separating success and error responses into their own methods.

--- src/main/java/com/agrusi/backendapi/handler/ResponseHandler.java
+++ src/main/java/com/agrusi/backendapi/handler/ResponseHandler.java
@@ -3,39 +3,43 @@ package com.agrusi.backendapi.handler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ResponseHandler {
 
-    public static ResponseEntity<Object> generateResponse(
-            HttpStatus status, String message, Object responseObj
+    public static ResponseEntity<Object> generateSuccessResponse(
+            HttpStatus httpStatus, String message, Object data
     ) {
         Map<String, Object> map = new LinkedHashMap<>();
+        map.put("status", "success");
 
-        try {
-            map.put("timestamp", new Date());
-            map.put("statusCode", status.value());
+        if (message != null) {
             map.put("message", message);
-
-            if (responseObj != null) {
-                map.put("data", responseObj);
-            }
-
-            return new ResponseEntity<>(map, status);
-
-        } catch (Exception e) {
-            map.clear();
-            map.put("timestamp", new Date());
-            map.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-            map.put("message", e.getMessage());
-
-            if (responseObj != null) {
-                map.put("data", responseObj);
-            }
-
-            return new ResponseEntity<>(map, status);
         }
+
+        if (data != null) {
+            map.put("data", data);
+        }
+
+        return new ResponseEntity<>(map, httpStatus);
+    }
+
+    public static ResponseEntity<Object> generateErrorResponse(
+            HttpStatus httpStatus, String message, Object error
+    ) {
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("status", "error");
+
+        if (message != null) {
+            map.put("message", message);
+        }
+
+        if (error != null) {
+            List<Object> errors = new ArrayList<>();
+            errors.add(error);
+            map.put("errors", errors);
+        }
+
+        return new ResponseEntity<>(map, httpStatus);
     }
 }
