fff6b67d1d8b806dc7ad235388c067e9ebbd3389
Juhani Rusi <juhanirusi@yahoo.com>
2024-06-29 01:11:05 +0300

    Edited the response DTO by adding "updatedAt" and "createdAt" fields to the DTO.

--- src/main/java/com/agrusi/backendapi/dto/response/account/AccountFullResponseDto.java
+++ src/main/java/com/agrusi/backendapi/dto/response/account/AccountFullResponseDto.java
@@ -1,13 +1,14 @@
-package com.agrusi.backendapi.dto.response;
+package com.agrusi.backendapi.dto.response.account;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record RegisterAccountResponseDto (
+public record AccountFullResponseDto(
         UUID publicId,
         String email,
         String firstName,
         String lastName,
         boolean accountVerified,
-        LocalDateTime dateCreated
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
 ) { }
