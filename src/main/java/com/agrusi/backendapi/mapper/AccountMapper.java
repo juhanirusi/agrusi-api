baee54618e33597e46e2ad8c8f723aeca5e27936
Juhani Rusi <juhanirusi@yahoo.com>
2024-07-01 17:31:49 +0300

    Edited the AccountMapper by adding more mapping DTOs and their target mapping fields.

--- src/main/java/com/agrusi/backendapi/mapper/AccountMapper.java
+++ src/main/java/com/agrusi/backendapi/mapper/AccountMapper.java
@@ -1,16 +1,28 @@
 package com.agrusi.backendapi.mapper;
 
-import com.agrusi.backendapi.dto.response.RegisterAccountResponseDto;
+import com.agrusi.backendapi.dto.response.account.AccountBasicResponseDto;
+import com.agrusi.backendapi.dto.response.account.AccountFullResponseDto;
+import com.agrusi.backendapi.dto.response.auth.AccountRegistrationResponseDto;
 import com.agrusi.backendapi.model.Account;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
+
+/*
+* componentModel = "spring" --> The generated mapper is a Spring bean
+* and can be retrieved via @Autowired
+*/
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
 
-    AccountMapper INSTANCE = Mappers.getMapper( AccountMapper.class );
+    @Mapping(source = "verified", target = "accountVerified")
+    AccountRegistrationResponseDto toAccountRegistrationResponseDto(Account account);
 
     @Mapping(source = "verified", target = "accountVerified")
-    RegisterAccountResponseDto toRegisterAccountResponseDto(Account account);
+    @Mapping(source = "dateCreated", target = "createdAt")
+    @Mapping(source = "lastUpdated", target = "updatedAt")
+    AccountFullResponseDto toAccountFullResponseDto(Account account);
+
+    @Mapping(source = "verified", target = "accountVerified")
+    AccountBasicResponseDto toAccountBasicResponseDto(Account account);
 }
