1728dd6d2a806cac315870bacfaafccd47ecbe71
Juhani Rusi <juhanirusi@yahoo.com>
2024-07-02 16:47:25 +0300

    Added a AccountRegistrationResponseDto that's returned when a user registers a new account.

---
+++ src/main/java/com/agrusi/backendapi/dto/response/auth/AccountRegistrationResponseDto.java
@@ -0,0 +1,11 @@
+package com.agrusi.backendapi.dto.response.auth;
+
+import java.util.UUID;
+
+public record AccountRegistrationResponseDto(
+        UUID publicId,
+        String email,
+        String firstName,
+        String lastName,
+        boolean accountVerified
+) { }
