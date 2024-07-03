c28e74b4fd1f4baeb340e1d86f04aa26bce44541
Juhani Rusi <juhanirusi@yahoo.com>
2024-07-03 16:43:48 +0300

    Added a repository findAccountByPublicId to find a user account by public ID and some comments.

--- src/main/java/com/agrusi/backendapi/repository/AccountRepository.java
+++ src/main/java/com/agrusi/backendapi/repository/AccountRepository.java
@@ -5,11 +5,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Optional<Account> findAccountByEmail(String email);
 
+    Optional<Account> findAccountByPublicId(UUID publicId);
+
     Boolean existsByEmail(String email);
+
+    /*
+    * Optional<MyEntity> findById(Long id);
+    *
+    * List<MyEntity> findByName(String name);
+    *
+    * List<MyEntity> findByAgeGreaterThan(int age);
+    *
+    * List<MyEntity> findByStatus(String status);
+    */
 }
