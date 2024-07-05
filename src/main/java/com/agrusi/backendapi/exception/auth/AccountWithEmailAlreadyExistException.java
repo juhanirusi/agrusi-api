3709de19434a06ece18154f024dc177e66e6344f
Juhani Rusi <juhanirusi@yahoo.com>
2024-07-06 00:24:37 +0300

    Edited the exception class by making it extend our "BaseCustomException" instead and creating two different constructors.

--- src/main/java/com/agrusi/backendapi/exception/auth/AccountWithEmailAlreadyExistException.java
+++ src/main/java/com/agrusi/backendapi/exception/auth/AccountWithEmailAlreadyExistException.java
@@ -1,12 +1,25 @@
 package com.agrusi.backendapi.exception.auth;
 
-public class AccountWithEmailAlreadyExistException extends RuntimeException {
+import com.agrusi.backendapi.exception.BaseCustomException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class AccountWithEmailAlreadyExistException extends BaseCustomException {
 
     public AccountWithEmailAlreadyExistException(final String email) {
-        super("Account with email '" + email + "' already exists.");
+        super(
+                HttpStatus.CONFLICT,
+                "Account creation failed.",
+                "Account with email address '" + email + "' already exists."
+        );
     }
 
     public AccountWithEmailAlreadyExistException() {
-        super("Account with this email already exists.");
+        super(
+                HttpStatus.CONFLICT,
+                "Account creation failed.",
+                "Account with the provided email address already exists."
+        );
     }
 }
