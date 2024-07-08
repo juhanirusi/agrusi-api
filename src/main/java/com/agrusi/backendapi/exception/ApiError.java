27f5a28805229984db8c5b7a24ba82d1ba7e61e1
Juhani Rusi <juhanirusi@yahoo.com>
2024-07-08 17:16:16 +0300

    Added the ApiError record that basically holds the structure of our error responses.

--- src/main/java/com/agrusi/backendapi/exception/ApiError.java
+++ src/main/java/com/agrusi/backendapi/exception/ApiError.java
@@ -2,4 +2,8 @@ package com.agrusi.backendapi.exception;
 
 import java.util.List;
 
-public record ApiError(String status, String message, List<?> errors) { }
+public record ApiError(
+        String status,
+        String message,
+        List<?> errors
+) { }
