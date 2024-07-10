447d1b1763406b2113f5d66ff50281c0908abd20
Juhani Rusi <juhanirusi@yahoo.com>
2024-07-10 17:47:50 +0300

    Edited the AuthController by moving the JSON response structure logic to ApiSuccessResponse and ResponseHandler.

--- src/main/java/com/agrusi/backendapi/controller/AuthController.java
+++ src/main/java/com/agrusi/backendapi/controller/AuthController.java
@@ -1,10 +1,9 @@
 package com.agrusi.backendapi.controller;
 
-import com.agrusi.backendapi.dto.request.LoginDto;
-import com.agrusi.backendapi.dto.request.RegisterDto;
-import com.agrusi.backendapi.dto.response.LoginResponseDto;
-import com.agrusi.backendapi.dto.response.RegisterAccountResponseDto;
-import com.agrusi.backendapi.exception.auth.AccountWithEmailAlreadyExistException;
+import com.agrusi.backendapi.dto.request.auth.LoginDto;
+import com.agrusi.backendapi.dto.request.auth.RegisterDto;
+import com.agrusi.backendapi.dto.response.auth.AccountRegistrationResponseDto;
+import com.agrusi.backendapi.dto.response.auth.LoginResponseDto;
 import com.agrusi.backendapi.handler.ResponseHandler;
 import com.agrusi.backendapi.service.AuthService;
 import jakarta.validation.Valid;
@@ -13,11 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @RestController
-@RequestMapping("api/v1/auth")
+@RequestMapping(value = "api/v1/auth")
 public class AuthController {
 
     private final AuthService authService;
@@ -28,50 +25,34 @@ public class AuthController {
 
     // Create (register) a new user REST API endpoint...
 
-    @PostMapping(value = "/register") // PERHAPS REPLACE <Object> WITH <?>
-    public ResponseEntity<Object> register(@Valid @RequestBody RegisterDto registerDto) {
+    @PostMapping(value = "/register")
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterDto registerDto) {
 
-        try {
-            RegisterAccountResponseDto createdAccount = authService.registerNewAccount(registerDto);
+        AccountRegistrationResponseDto account = authService.registerNewAccount(registerDto);
 
-            return ResponseHandler.generateResponse(
-                    HttpStatus.CREATED,
-                    "success",
-                    "Account created successfully.",
-                    new LinkedHashMap<>() {{
-                        put("publicId", createdAccount.publicId());
-                        put("email", createdAccount.email());
-                        put("firstName", createdAccount.firstName());
-                        put("lastName", createdAccount.lastName());
-                        put("accountVerified", createdAccount.accountVerified());
-                    }}
-            );
-        } catch (AccountWithEmailAlreadyExistException exception) {
-
-            return ResponseHandler.generateResponse(
-                    HttpStatus.CONFLICT,
-                    "error",
-                    "Account creation failed",
-                    Map.of("message", exception.getMessage())
-            );
-        }
+        return ResponseHandler.generateSuccessResponse(
+                HttpStatus.CREATED,
+                "Account created successfully.",
+                account
+        );
     }
 
     // Log in an existing user REST API endpoint...
+    // TODO --> COMPLETE THIS BY ADDING ERROR HANDLING...
 
     @PostMapping(value = "/login")
-    public ResponseEntity<Object> login(@Valid @RequestBody LoginDto loginDto) {
+    public ResponseEntity<?> login(@Valid @RequestBody LoginDto loginDto) {
 
         LoginResponseDto loggedInAccount = authService.login(loginDto);
 
-        return ResponseHandler.generateResponse(
+        return ResponseHandler.generateSuccessResponse(
                 HttpStatus.OK,
-                "success",
                 "Login successful.",
-                new LinkedHashMap<>() {{
-                    put("email", loggedInAccount.email());
-                    put("accessToken", loggedInAccount.token());
-                }}
+                loggedInAccount
+//                new LinkedHashMap<>() {{
+//                    put("email", loggedInAccount.email());
+//                    put("accessToken", loggedInAccount.token()); // TODO --> CHANGE NAME OF FIELD !!!
+//                }}
         );
     }
 
@@ -80,7 +61,7 @@ public class AuthController {
         return "YOU GET TO THE PAGE";
     }
 
-    @GetMapping("/user")
+    @GetMapping(value = "/user")
     public Principal user(Principal user) {
         return user;
     }
