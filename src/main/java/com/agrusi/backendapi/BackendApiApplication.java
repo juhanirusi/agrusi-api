852b7244320d8f2d36931e3e8fbf3bb9a049de5d
Juhani Rusi <juhanirusi@yahoo.com>
2024-07-13 11:50:35 +0300

    Removed useless code.

--- src/main/java/com/agrusi/backendapi/BackendApiApplication.java
+++ src/main/java/com/agrusi/backendapi/BackendApiApplication.java
@@ -10,29 +10,4 @@ public class BackendApiApplication {
 		SpringApplication.run(BackendApiApplication.class, args);
 	}
 
-//	@Bean
-//	CommandLineRunner run(
-//			RoleRepository roleRepository
-//	) {
-//		return args -> {
-//
-//			if (roleRepository.findByAuthority(EAccountRole.ADMIN).isPresent()) {
-//				System.out.println("ADMIN role already present in the database");
-//			} else {
-//				roleRepository.save(new Role(EAccountRole.ADMIN));
-//			}
-//
-//			if (roleRepository.findByAuthority(EAccountRole.MODERATOR).isPresent()) {
-//				System.out.println("MODERATOR role already present in the database");
-//			} else {
-//				roleRepository.save(new Role(EAccountRole.MODERATOR));
-//			}
-//
-//			if (roleRepository.findByAuthority(EAccountRole.USER).isPresent()) {
-//				System.out.println("USER role already present in the database");
-//			} else {
-//				roleRepository.save(new Role(EAccountRole.USER));
-//			}
-//		};
-//	}
 }
