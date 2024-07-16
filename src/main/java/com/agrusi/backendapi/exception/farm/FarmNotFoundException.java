0a9c4a6c027d55539e7a4d0670af437cdef699f2
Juhani Rusi <juhanirusi@yahoo.com>
2024-07-16 16:42:48 +0300

    Added Edited the FarmNotFoundException to extend our BaseCustomException and created two different exception messages to throw.

--- src/main/java/com/agrusi/backendapi/exception/farm/FarmNotFoundException.java
+++ src/main/java/com/agrusi/backendapi/exception/farm/FarmNotFoundException.java
@@ -1,14 +1,27 @@
 package com.agrusi.backendapi.exception.farm;
 
+import com.agrusi.backendapi.exception.BaseCustomException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.util.UUID;
 
-public class FarmNotFoundException extends RuntimeException {
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class FarmNotFoundException extends BaseCustomException {
 
     public FarmNotFoundException(UUID publicFarmId) {
-        super("Farm with public ID '" + publicFarmId + "' not found.");
+        super(
+                HttpStatus.NOT_FOUND,
+                "Farm not found.",
+                "Farm with public ID '" + publicFarmId + "' not found."
+        );
     }
 
-    public FarmNotFoundException(String message) {
-        super(message);
+    public FarmNotFoundException() {
+        super(
+                HttpStatus.NOT_FOUND,
+                "Farm not found.",
+                "Farm with the provided public ID not found."
+        );
     }
 }
