ad7f1bce7e3d1e50eeb158caf26e4497bbeefcc7
Juhani Rusi <juhanirusi@yahoo.com>
2024-07-17 16:09:38 +0300

    Added FarmPostDto that's used as a DTO class when performing POST HTTP requests on the Farm entity... when a user creates a new farm.

---
+++ src/main/java/com/agrusi/backendapi/dto/request/farm/FarmPostDto.java
@@ -0,0 +1,33 @@
+package com.agrusi.backendapi.dto.request.farm;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class FarmPostDto {
+
+    @NotBlank(message = "Farm name is mandatory.")
+    @NotEmpty(message = "Email can't be empty.")
+    @Size(
+            min = 2,
+            max = 255,
+            message = "Farm name is required and needs to be " +
+                    "between {min} and {max} characters long."
+    )
+    private String farmName;
+
+    public FarmPostDto() {
+    }
+
+    public FarmPostDto(String farmName) {
+        this.farmName = farmName;
+    }
+
+    public String getFarmName() {
+        return farmName;
+    }
+
+    public void setFarmName(String farmName) {
+        this.farmName = farmName;
+    }
+}
