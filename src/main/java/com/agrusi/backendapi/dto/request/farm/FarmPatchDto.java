715ec9799522ab56d4bbc7d9cd39b2cca48142ef
Juhani Rusi <juhanirusi@yahoo.com>
2024-07-17 16:08:55 +0300

    Added FarmPatchDto that's used as a DTO class when performing PATCH HTTP requests on the Farm entity.

---
+++ src/main/java/com/agrusi/backendapi/dto/request/farm/FarmPatchDto.java
@@ -0,0 +1,31 @@
+package com.agrusi.backendapi.dto.request.farm;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.Size;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class FarmPatchDto {
+
+    @Size(
+            min = 2,
+            max = 255,
+            message = "Farm name needs to be " +
+                    "between {min} and {max} characters long."
+    )
+    private String farmName;
+
+    public FarmPatchDto() {
+    }
+
+    public FarmPatchDto(String farmName) {
+        this.farmName = farmName;
+    }
+
+    public String getFarmName() {
+        return farmName;
+    }
+
+    public void setFarmName(String farmName) {
+        this.farmName = farmName;
+    }
+}
