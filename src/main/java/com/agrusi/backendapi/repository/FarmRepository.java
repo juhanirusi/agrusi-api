5781847a460e52b0bc168524f5a53a8348871a18
Juhani Rusi <juhanirusi@yahoo.com>
2024-07-18 15:18:22 +0300

    Added "existsByPublicId" and "findByPublicId" methods to the FarmRepository interface.

--- src/main/java/com/agrusi/backendapi/repository/FarmRepository.java
+++ src/main/java/com/agrusi/backendapi/repository/FarmRepository.java
@@ -2,6 +2,15 @@ package com.agrusi.backendapi.repository;
 
 import com.agrusi.backendapi.model.Farm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
 public interface FarmRepository extends JpaRepository<Farm, Long> {
+
+    Boolean existsByPublicId(UUID publicId);
+
+    Optional<Farm> findByPublicId(UUID publicId);
 }
