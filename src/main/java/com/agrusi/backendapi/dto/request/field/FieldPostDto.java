88e631e26c8fd26284b7f0d7869f2e42c70fc0b2
Juhani Rusi <juhanirusi@yahoo.com>
2024-07-22 19:29:16 +0300

    Added FieldPostDto that's used as a DTO when users perform HTTP POST requests by creating a new field.

---
+++ src/main/java/com/agrusi/backendapi/dto/request/field/FieldPostDto.java
@@ -0,0 +1,48 @@
+package com.agrusi.backendapi.dto.request.field;
+
+import com.agrusi.backendapi.validator.annotation.ValidCoordinates;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+public class FieldPostDto {
+
+    @NotBlank(message = "Field name is mandatory.")
+    @NotEmpty(message = "Field name can't be empty.")
+    @Size(
+            min = 2,
+            max = 255,
+            message = "Field name is required and needs to be " +
+                    "between {min} and {max} characters long.."
+    )
+    private String name;
+
+    @ValidCoordinates
+    private List<List<List<Double>>> coordinates;
+
+    public FieldPostDto(
+            String name,
+            List<List<List<Double>>> coordinates
+    ) {
+        this.name = name;
+        this.coordinates = coordinates;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<List<List<Double>>> getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(List<List<List<Double>>> coordinates) {
+        this.coordinates = coordinates;
+    }
+}
