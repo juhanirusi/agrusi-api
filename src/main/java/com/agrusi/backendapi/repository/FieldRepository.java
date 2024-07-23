0a51943e9880253ed9c8637d41906f663fe43ad2
Juhani Rusi <juhanirusi@yahoo.com>
2024-07-23 15:33:43 +0300

    Added FieldRepository interface containing the methods for working with the Field entity.

--- src/main/java/com/agrusi/backendapi/repository/FieldRepository.java
+++ src/main/java/com/agrusi/backendapi/repository/FieldRepository.java
@@ -1,7 +1,14 @@
 package com.agrusi.backendapi.repository;
 
+import com.agrusi.backendapi.model.Farm;
 import com.agrusi.backendapi.model.Field;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface FieldRepository extends JpaRepository<Field, Long> {
+
+    Optional<Field> findByIdAndFarm(Long id, Farm farm);
 }
