bac3b82a4b7cd4ca9e7ba57d8b94e96a95c9ca3a
Juhani Rusi <juhanirusi@yahoo.com>
2024-08-13 18:20:37 +0300

    Added a sql file used for testing that adds roles to the testing database.

---
+++ src/test/resources/data-test.sql
@@ -0,0 +1,5 @@
+-- Insert the needed roles to the database...
+
+INSERT INTO role (id, authority) VALUES (1, 'ADMIN');
+INSERT INTO role (id, authority) VALUES (2, 'MODERATOR');
+INSERT INTO role (id, authority) VALUES (3, 'USER');
